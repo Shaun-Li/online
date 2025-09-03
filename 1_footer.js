121031a791c2496b5f53ec19ce6fc3cc31816428
Shaun Li <46808301+Shaun-Li@users.noreply.github.com>
2025-09-03 21:38:26 +0800

    Update 1_footer.js

--- 1_footer.js
+++ 1_footer.js
@@ -34,7 +34,6 @@ async function cbc(key, ivStr, plaintext) {
     }
 }
 const apiUrl = 'https://master-data-admin-uat.test.lcscm.cn/master-api/userCp/getUserInfo';
-window.a1 = '';
 const reqFn = async () => {
     try {
         const response = await fetch(apiUrl, {
@@ -52,37 +51,26 @@ const reqFn = async () => {
         const responseData = await response.json();
 
         if (responseData.success && responseData.result && typeof responseData.result.userId !== 'undefined') {
-            window.a1 = responseData.result.userId; 
+            const a1 = responseData.result.userId;
+            window.ed = async function (data) {
+                const date = (new Date()).getTime();
+
+                const data1 = { currentDate: date+''+date };
+                const data2 = { ...data, currentDate: (new Date()).getTime(), a1 };
+
+                const data1Str =JSON.stringify(data1);
+                const data2Str =JSON.stringify(data2);
+
+                const key = await gk(data1Str);
+
+                const encrypt = await cbc(key, data1Str, data2Str);
+                const e = await cbc(key, data1Str, encrypt);
+
+                return { ...data, e, c1: date };
+            }
         }
     } catch (error) {
         console.error('请求失败:', error.message);
     }
 };
-
-window.ed = async function (data) {
-    try{
-        if(!window.a1) {
-            await reqFn();
-        }
-        const date = +new Date();
-
-        const data1 = { currentDate: date+''+date };
-        const data2 = Object.assign({}, data, {
-            currentDate: date,
-            a1: window.a1
-        });
-
-        const data1Str =JSON.stringify(data1);
-        const data2Str =JSON.stringify(data2);
-
-        const key = await gk(data1Str);
-
-        const encrypt = await cbc(key, data1Str, data2Str);
-        const e = await cbc(key, data1Str, encrypt);
-
-        return Object.assign({}, data, {e, c1: date});
-    } catch (error) {
-        console.error('加密失败:', error.message);
-        return data;
-    } 
-}
+reqFn()
